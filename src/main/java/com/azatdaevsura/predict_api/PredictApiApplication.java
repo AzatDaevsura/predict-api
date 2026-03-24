0332e61ea8214994003fb19bf17008ae11cc3c10
Azat Oezden <213454142+AzatDaevsura@users.noreply.github.com>
2026-03-24 23:12:30 +0100

    start commit - init

---
+++ src/main/java/com/azatdaevsura/predict_api/PredictApiApplication.java
@@ -0,0 +1,14 @@
+package com.azatdaevsura.predict_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictApiApplication.class, args);
+	}
+
+
+}
