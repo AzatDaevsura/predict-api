5c66f44aecca336cb86a9b3eed96cb9d7e14aa87
Azat Oezden <213454142+AzatDaevsura@users.noreply.github.com>
2026-03-28 18:54:42 +0100

    implement placeholder predict_results, savePrediction, and loadModel methods in PredictionService

--- src/main/java/com/azatdaevsura/predict_api/logic/PredictionService.java
+++ src/main/java/com/azatdaevsura/predict_api/logic/PredictionService.java
@@ -1,7 +1,22 @@
 package com.azatdaevsura.predict_api.logic;
 
+import com.azatdaevsura.predict_api.model.Prediction_Results;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PredictionService implements PredictionService_Interface {
+    @Override
+    public Prediction_Results predict_results() {
+        return null;
+    }
+
+    @Override
+    public void savePrediction(String prediction) {
+
+    }
+
+    @Override
+    public void loadModel(String modelPath) {
+
+    }
 }
