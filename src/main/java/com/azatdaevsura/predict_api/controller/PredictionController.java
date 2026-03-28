2dec8b5b7dcdd14a30529c09521107d7bea2dbdf
Azat Oezden <213454142+AzatDaevsura@users.noreply.github.com>
2026-03-28 17:48:12 +0100

    add PredictionController and PredictionService for prediction API (only structure v0.1)

---
+++ src/main/java/com/azatdaevsura/predict_api/controller/PredictionController.java
@@ -0,0 +1,21 @@
+package com.azatdaevsura.predict_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PredictionController {
+
+    @GetMapping("/predict")
+    public String predict() {
+        return "This is a prediction result.";
+    }
+
+    @PostMapping ("/user")
+    public String createUser() {
+        return "User created successfully.";
+    }
+}
---
+++ src/main/java/com/azatdaevsura/predict_api/logic/PredictionService.java
@@ -0,0 +1,7 @@
+package com.azatdaevsura.predict_api.logic;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PredictionService {
+}
