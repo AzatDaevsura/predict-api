cb1ccb9913a91dccc43c9045806839bbe792b6b8
Azat Oezden <213454142+AzatDaevsura@users.noreply.github.com>
2026-04-19 19:23:42 +0200

    add deleteUser endpoint to UserController and integrate UserService for user management

--- src/main/java/com/azatdaevsura/predict_api/controller/UserController.java
+++ src/main/java/com/azatdaevsura/predict_api/controller/UserController.java
@@ -1,31 +1,39 @@
 package com.azatdaevsura.predict_api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.azatdaevsura.predict_api.logic.UserService;
+import com.azatdaevsura.predict_api.model.UserModel;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
 public class UserController {
+    UserService userService = new UserService();
 
     @PostMapping("/user")
-    public String createUser() {
+    public String createUser(@RequestBody UserModel user) {
+        //userService.createUser(user.getUserName(), user.getStudyHours(), user.getSleepHours(), user.getPhysicalActivityHours(), user.getDietQuality(), user.getStressLevel());
 
         return "User created successfully.";
     }
 
     @GetMapping("/users")
     public  String getAllUsers() {
+        userService.getAllUsers();
         return "List of Users";
     }
     @GetMapping("/user/{id}")
     public String getUserById() {
+        userService.getUserById(UserModel.userIdCounter);
         return "User details for ID: {id}";
     }
     @GetMapping("user/{id}/prediction")
     public String getPredictionForUser() {
         return "Prediction result for user with ID: {id}";
+    }
+    @DeleteMapping("/user/{id}")
+    public String deleteUser() {
+        userService.deleteUser(UserModel.userIdCounter);
+        return "User with ID: {id} deleted successfully.";
     }
 
 
